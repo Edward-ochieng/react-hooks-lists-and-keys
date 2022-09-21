3fafbabc2ac72e1d23fbceb452a1294442e45043
Edward <edward.ochieng@student.moringaschool.com>
2022-09-21 09:46:14 +0300

    completed lab

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -7,17 +7,27 @@ function ColorList() {
     "salmon",
     "darkslategray",
     "hotpink",
+    "black"
   ];
+   const colorElements = colors.map((color) => {
+    return (
+      <li key={color} style={{color: color}}>
+      </li>
+    );
+  });
 
   return (
     <div>
       <h1>Top 5 CSS Colors</h1>
       <ol>
+        {/* display the array of <li> elements here! */}
         <li style={{ color: colors[0] }}>{colors[0]}</li>
         <li style={{ color: colors[1] }}>{colors[1]}</li>
         <li style={{ color: colors[2] }}>{colors[2]}</li>
         <li style={{ color: colors[3] }}>{colors[3]}</li>
         <li style={{ color: colors[4] }}>{colors[4]}</li>
+        <li style={{ color: colors[5]}}>{colors[5]}</li>
+        {colorElements}
       </ol>
     </div>
   );
